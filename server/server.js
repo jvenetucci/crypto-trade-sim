32bef57f587062e40213b3f134f63d7f0c5ad4d9
Joseph Venetucci <venetuc@pdx.edu>
2018-08-18 14:15:01 -0700

    setup Winston for logging

--- server/server.js
+++ server/server.js
@@ -2,38 +2,55 @@
 
 const express = require('express');
 const sqlite3 = require('sqlite3');
+const winston = require('winston');
 const crypto = require('crypto');
 
 
+const logFormat = winston.format.printf(info => {
+    return info.timestamp +  " [" + info.level + "]: " + info.message;
+});
+
+const logger = winston.createLogger({
+    transports: [
+        new winston.transports.Console({
+            level: 'info',
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.timestamp({format: 'MM-DD-YYYY HH:mm:ss'}),
+                logFormat
+            )
+        }),
+    ]
+});
 
 // const server = express();
 
 // Currently only open the DB if it exists, don't attempt to create it.
 const db = new sqlite3.Database('./db/crypto-ts-db.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
-        console.error("Unable to find the database file:");
-        console.error(err.message);
+        logger.error("Unable to find the database file:");
+        logger.error(err.message);
     } else {
-        console.log("Successfully connected to database.");
+        logger.info("Successfully connected to database.");
     }
 })
 
 // db.all("SELECT name FROM sqlite_master WHERE type='table';", (err, rows) => {
 //     if (err) {
-//         console.error(err.message);
+//         logger.error(err.message);
 //     } else {
 //         rows.forEach((row) => {
-//             console.log(row);
+//             logger.info(row);
 //         });
 //     }
 // })
 
 // db.all("SELECT * FROM Users;", (err, rows) => {
 //     if (err) {
-//         console.error(err.message);
+//         logger.error(err.message);
 //     } else {
 //         rows.forEach((row) => {
-//             console.log(row);
+//             logger.info(row);
 //         });
 //     }
 // })
@@ -48,29 +65,29 @@ hash.update(password + salt);
 
 var hashedPassword = hash.digest('hex');
 
-console.log(username);
-console.log(password);
-console.log(salt);
-console.log(password+salt);
-console.log(hashedPassword);
+logger.info(username);
+logger.info(password);
+logger.info(salt);
+logger.info(password+salt);
+logger.info(hashedPassword);
 
-db.run('INSERT INTO Users VALUES (?, ?, ?)', [username, hashedPassword, salt], function (err) {
-    if (err) {
-        console.error(err.message);
-    } else {
-        console.log('Added new user to User table row ' + this.lastID);
-    }
-})
+// db.run('INSERT INTO Users VALUES (?, ?, ?)', [username, hashedPassword, salt], function (err) {
+//     if (err) {
+//         logger.error(err.message);
+//     } else {
+//         logger.info('Added new user to User table row ' + this.lastID);
+//     }
+// })
 
 db.close((err) => {
     if (err) {
-        console.error("Unable to close the database file:");
-        console.error(err.message);
+        logger.error("Unable to close the database file:");
+        logger.error(err.message);
     } else {
-        console.log('Close the database connection.');
+        logger.info('Close the database connection.');
     }
 });
 
 // server.get('/', (req, res) => res.send('Hello World!'));
 
-// server.listen(3001, () => console.log('Example app listening on port 3001!'));
+// server.listen(3001, () => logger.info('Example app listening on port 3001!'));
