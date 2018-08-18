02c7e5d43e9bc11c65c874c3da34859d51a3736e
Joseph Venetucci <venetuc@pdx.edu>
2018-08-18 13:04:10 -0700

    password hashing + salt into database

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@
 
 const express = require('express');
 const sqlite3 = require('sqlite3');
+const crypto = require('crypto');
+
+
 
 // const server = express();
 
@@ -15,23 +18,47 @@ const db = new sqlite3.Database('./db/crypto-ts-db.db', sqlite3.OPEN_READWRITE, 
     }
 })
 
-db.all("SELECT name FROM sqlite_master WHERE type='table';", (err, rows) => {
-    if (err) {
-        console.error(err.message);
-    } else {
-        rows.forEach((row) => {
-            console.log(row);
-        });
-    }
-})
+// db.all("SELECT name FROM sqlite_master WHERE type='table';", (err, rows) => {
+//     if (err) {
+//         console.error(err.message);
+//     } else {
+//         rows.forEach((row) => {
+//             console.log(row);
+//         });
+//     }
+// })
 
-db.all("SELECT * FROM Users;", (err, rows) => {
+// db.all("SELECT * FROM Users;", (err, rows) => {
+//     if (err) {
+//         console.error(err.message);
+//     } else {
+//         rows.forEach((row) => {
+//             console.log(row);
+//         });
+//     }
+// })
+
+var username = "UserA";
+var password = "password";
+var salt = crypto.randomBytes(8).toString('hex');
+
+var hash = crypto.createHash('sha256');
+
+hash.update(password + salt);
+
+var hashedPassword = hash.digest('hex');
+
+console.log(username);
+console.log(password);
+console.log(salt);
+console.log(password+salt);
+console.log(hashedPassword);
+
+db.run('INSERT INTO Users VALUES (?, ?, ?)', [username, hashedPassword, salt], function (err) {
     if (err) {
         console.error(err.message);
     } else {
-        rows.forEach((row) => {
-            console.log(row);
-        });
+        console.log('Added new user to User table row ' + this.lastID);
     }
 })
 
