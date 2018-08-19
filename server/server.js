5e1865d182d53da85467b40cc92e7ae7c9c0ad88
Joseph Venetucci <venetuc@pdx.edu>
2018-08-18 21:09:44 -0700

    Added buy endpoint

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@
 const express = require('express');
 const sqlite3 = require('sqlite3');
 
-const winston = require('winston');
 const morgan = require('morgan');
+const winston = require('winston');
 
 const logFormat = winston.format.printf(info => {
     return info.timestamp +  " [" + info.level + "]: " + info.message;
@@ -131,6 +131,90 @@ server.post('/login', jsonParser, (req, res) => {
         })
     }
 });
+
+/**
+ * Buy an amount of cryptocurrency for USD
+ * Request Body:
+ *  Content-Type: application/json
+ *  Required Fields: {username, currency, quantity, price}
+ * Response:
+ *  200 - Good login
+ *  400 - Missing required fields in request body
+ *  404 - User or USD holdings can't be found
+ *  409 - Not Enough money
+ *  500 - Something went wrong with the DB
+ */
+server.post('/buy', jsonParser, (req, res) => {
+    // If the request is missing the required params, send 400
+    if (!req.body.username || !req.body.quantity || !req.body.currency || !req.body.price) { 
+        res.status(400).send("Missing required fields in body");
+    } else {
+        logger.info(req.body.username +" attempting to buy " + req.body.quantity +
+            " " + req.body.currency + " for " + req.body.price);
+        db.get('SELECT quantity FROM Holdings WHERE username = ? AND currency = ?', [req.body.username, "USD"], function (err, row) {
+            if (err) {
+                logger.error(err.message);
+                res.status(500).send("Something happened to the DB, check server logs...");
+            } else {
+                if (row) {
+                    console.log(row);
+                    if (row.quantity < req.body.price) {
+                        // Not enough funds
+                        logger.info("\t" + "Buy amount is " + req.body.price + " but user only has " + row.quantity);
+                        res.status(409).send("Not enough funds to purchase"); 
+                    } else {
+                        logger.info("\t" + "purchasing...");
+                        var newBalance = row.quantity - req.body.price;
+                        db.serialize(() => {
+                            db.run('BEGIN TRANSACTION');
+                            db.run('INSERT INTO Transactions VALUES (?, ?, ?, ?, ?, ?, ?)',
+                              [req.body.username, Date.now(), "BUY", req.body.currency, "USD", req.body.quantity, req.body.price], (err) => {
+                                if (err) {
+                                    db.run('ROLLBACK');
+                                    logger.error(err.message);
+                                    res.status(500).send("Something happened to the DB, check server logs...");
+                                }
+                            });
+                            db.run('UPDATE Holdings SET quantity = quantity + ? WHERE username = ? AND currency = ?',
+                             [req.body.quantity, req.body.username, req.body.currency], function(err) {
+                                if (err) {
+                                    db.run('ROLLBACK');
+                                    logger.error(err.message);
+                                    res.status(500).send("Something happened to the DB, check server logs...");
+                                } else if (this.changes == 0) {
+                                    db.run('INSERT INTO Holdings VALUES (?, ?, ?)', 
+                                      [req.body.username, req.body.currency, req.body.quantity], (err) => {
+                                        if (err) {
+                                            db.run('ROLLBACK');
+                                            logger.error(err.message);
+                                            res.status(500).send("Something happened to the DB, check server logs...");
+                                        } 
+                                    })
+                                }
+                            });
+                            db.run('UPDATE Holdings SET quantity = ? WHERE username = ? AND currency = ?', 
+                              [newBalance, req.body.username, "USD"], (err) => {
+                                if (err) {
+                                    db.run('ROLLBACK');
+                                    logger.error(err.message);
+                                    res.status(500).send("Something happened to the DB, check server logs...");
+                                }
+                            });
+                            db.run('COMMIT');
+                        })
+                        logger.info("\t" + "purchase successful");
+                        res.status(200).send("Purchase successful");
+                    }
+                } else {
+                    // User does not exist, or no existing record for USD
+                    logger.info("\t" + "Can't get USD holdings for " + req.body.username);
+                    res.status(404).send("Cannot find user or user's USD holdings");
+                }
+            }
+        })
+    }
+});
+
 
 /**
  * Retrieve an array of a users current holdings with current values. Will send an empty array if the user does not exist.
