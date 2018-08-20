01072865bb0f957576beb7afce2b02eef4b08262
Joseph Venetucci <venetuc@pdx.edu>
2018-08-19 22:04:48 -0700

    Tabs for register and login

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,48 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Container, Divider, Grid, Header, Image, Menu, Segment } from 'semantic-ui-react'
+import { Tab, Container, Divider, Grid, Header, Image, Menu, Segment } from 'semantic-ui-react'
 import LoginWindow from "./../LoginWindow";
+import RegisterWindow from "./../RegisterWindow";
+import '../../styles/LoginPage.css';
 
+const panes = [
+    {menuItem: 'Login', render: () => <Tab.Pane><LoginWindow /></Tab.Pane>},
+    {menuItem: 'Register', render: () => <Tab.Pane><RegisterWindow /></Tab.Pane>},
+]
 const Login = () => (
     <div>
-        <Container fluid textAlign='center' style={{ background: 'black', padding: '2em' }}>
+        <Container fluid textAlign='center' className='Header'>
             <Header inverted as='h1'>Crypto Trade Sim</Header>
-
             <Header inverted as='h2'>
                 A cryptocurrency buying simulator
             </Header>
         </Container>
 
-        <Grid centered>
+        <Grid centered classname='Content'>
             <Grid.Column width={8}>
-                <LoginWindow />
+                <Segment raised>
+                    <Tab panes={panes} />
+                </Segment>
             </Grid.Column>
         </Grid>
-
-        <Segment inverted vertical style={{ padding: '5em 0em'}}>
-        <Container>
-        <Grid divided inverted stackable>
-          <Grid.Row>
-            <Grid.Column width={3}>
-              <Header inverted as='h4' content='About' />
-              
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Header inverted as='h4' content='Services' />
-              
-            </Grid.Column>
-            <Grid.Column width={7}>
-              <Header as='h4' inverted>
-                Footer Header
-              </Header>
-              <p>
-                Extra space for a call to action inside the footer that could help re-engage users.
-              </p>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Container>
-    </Segment>
     </div>
 )
 
